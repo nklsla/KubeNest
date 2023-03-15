402d0e1fa515834740c43c3343f899f81978ce2a
Niklas Larsson <niklaslarsson500@hotmail.com>
2023-03-15 21:20:43 +0000

    Added local PV for docker registry

--- shutdown-registry.sh
+++ shutdown-registry.sh
@@ -1,5 +1,7 @@
-kubectl delete deployments.apps/private-repository-k8s
 kubectl delete service/docker-registry
-kubectl delete secret cert-secret auth-secret
-#kubectl delete pvc/docker-repo-pvc
-#kubectl delete pv/docker-repo-pv
+kubectl delete deployments.apps/private-repository-k8s
+kubectl delete secret cert-secret auth-secret docker-registry-secret
+kubectl delete pvc/docker-repo-pvc
+kubectl delete pv/docker-repo-pv
+kubectl delete storageclasses.storage.k8s.io/local-storage 
+
