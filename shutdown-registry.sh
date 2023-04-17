5413d90c3929393ffa1fc0c55160ef604f3824ca
Niklas Larsson <niklaslarsson500@hotmail.com>
2023-04-17 18:43:17 +0000

    added monitoring, cleaned up folders

--- shutdown-registry.sh
+++ shutdown-registry.sh
@@ -1,7 +1,8 @@
-kubectl delete service/docker-registry
-kubectl delete deployments.apps/local-img-repo
-kubectl delete secret cert-secret auth-secret docker-registry-secret
-kubectl delete pvc/docker-repo-pvc
+namespace=registry
+kubectl -n $namespace delete service/image-registry
+kubectl -n $namespace delete deployments.apps/image-registry
+kubectl -n $namespace delete secret cert-secret auth-secret docker-registry-secret
+kubectl -n $namespace delete pvc/docker-repo-pvc
 kubectl delete pv/docker-repo-pv
 kubectl delete storageclasses.storage.k8s.io/local-storage 
-
+kubectl delete namespaces $namespace
--- start-registry.sh
+++ start-registry.sh
@@ -1,15 +1,15 @@
-# Add secrets
-kubectl create secret tls cert-secret --cert=/srv/registry/cert/tls.crt --key=/srv/registry/cert/tls.key
-kubectl create secret generic auth-secret --from-file=/srv/registry/auth/htpasswd
-kubectl create secret docker-registry docker-registry-secret --docker-server=docker-registry:5000 --docker-username=myuser --docker-password=mypasswd
-
 # Remove taint on control-plane
 kubectl taint node eva node-role.kubernetes.io/control-plane-
 
-# Add storage label on eva
+# Add storage label on eva and create namespace
 kubectl label nodes eva nodetype=storage
+kubectl create namespace registry
+
+# Add secrets
+kubectl create secret tls cert-secret --cert=/srv/registry/cert/tls.crt --key=/srv/registry/cert/tls.key --namespace=registry
+kubectl create secret generic auth-secret --from-file=/srv/registry/auth/htpasswd --namespace=registry
+kubectl create secret docker-registry docker-registry-secret --docker-server=docker-registry:5000 --docker-username=myuser --docker-password=mypasswd --namespace=registry
 
 # Create Persistent volume, claim, deployment and service
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-kubectl create -f $DIR/manifests/registry-volume.yaml
-kubectl create -f $DIR/manifests/registry-service-deploy.yaml
+kubectl create -f $DIR/manifests/docker-registry
