269c789e5079697cf04c0b79e0c140368c0c3133
Niklas Larsson <niklaslarsson500@hotmail.com>
2023-04-19 18:40:26 +0000

    Added persitent volume for grafan, made sure internal service access works as intended

--- start-monitoring.sh
+++ start-monitoring.sh
@@ -1,6 +1,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 kubectl create namespace monitoring
 kubectl create -f $DIR/manifests/prometheus
-#kubectl create -f $DIR/manifests/kube-state-metrics
-#kubectl create -f $DIR/manifests/grafana
+kubectl create -f $DIR/manifests/kube-state-metrics
+kubectl create -f $DIR/manifests/grafana
 
