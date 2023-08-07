0beace9dd63f405f67393c8b36acdb7878925030
Niklas Larsson <niklaslarsson500@hotmail.com>
2023-08-07 19:46:45 +0000

    monitoring can now run on any node

--- scripts/shutdown-metrics-server.sh
+++ scripts/shutdown-metrics-server.sh
@@ -1,3 +1,5 @@
 # Shutdown the metrics-server
-kubectl -n kube-system delete deployments.apps metrics-server
-kubectl -n kube-system delete svc metrics-server
+namespace=kube-system
+kubectl -n $namespace delete deployments.apps metrics-server
+kubectl -n $namespace delete svc metrics-server
+kubectl -n $namespace delete serviceaccounts kube-state-metrics 
--- scripts/shutdown-monitoring.sh
+++ scripts/shutdown-monitoring.sh
@@ -1,12 +1,8 @@
 namespace=monitoring
-namespace2=kube-system
 kubectl -n $namespace delete service/prometheus-service service/grafana-service
-kubectl -n $namespace2 delete service/kube-state-metrics-service
 kubectl -n $namespace delete deployments.apps/prometheus deployments.apps/grafana 
-kubectl -n $namespace2 delete deployments.apps/kube-state-metrics
 kubectl -n $namespace delete pvc/prometheus-pvc pvc/grafana-pvc
-kubectl delete pv/prometheus-pv pv/grafana-pv
-kubectl delete clusterrole prometheus kube-state-metrics
-kubectl delete clusterrolebindings.rbac.authorization.k8s.io prometheus kube-state-metrics
-kubectl -n $namespace2 delete serviceaccounts kube-state-metrics 
+#kubectl delete pv/prometheus-pv pv/grafana-pv
+kubectl delete clusterrole prometheus #kube-state-metrics
+kubectl delete clusterrolebindings.rbac.authorization.k8s.io prometheus #kube-state-metrics
 kubectl delete namespaces $namespace
